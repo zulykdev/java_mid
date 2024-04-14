9b9fff6300557154f136e0e26f70b0cf784a7bd9
Hans Villaizan <zolymo@gmail.com>
2024-04-14 15:00:34 -0500

    Initial project

---
+++ src/main/java/com/brixton/javamid/JavaMidApplication.java
@@ -0,0 +1,13 @@
+package com.brixton.javamid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brixton/javamid/JavaMidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brixton.javamid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
