a5954cab881ffb9209c66891691b71e92fdc9143
Hans Villaizan <zolymo@gmail.com>
2024-04-17 23:07:56 -0500

    creando objetos anonimos, porque se crean en el parametro a enviar

--- src/main/java/com/brixton/javamid/herencia/Main.java
+++ src/main/java/com/brixton/javamid/herencia/Main.java
@@ -13,14 +13,23 @@ public class Main {
         log.info("PERSONA 1 antes de colocar datos"+ persona1);
         persona1.setNombre("ISA");
         persona1.setApellido("N");
-        LocalDate fechaNacimientoDePersona1 = LocalDate.now(); //17/04/2024 22:50
+        //LocalDate fechaNacimientoDePersona1 = LocalDate.now(); //17/04/2024 22:50
+        //LocalDate fechaNacimientoDePersona1A = LocalDate.of(2000,5,15); //15/05/2000
+        //fecha fechaNacimiento = fecha(2000,15,05)
+
+        // setFecha(fechaNacimiento)
+        // setFecha(fecha(2000,15,05))
+
+        //persona1.setFechaNacimiento(fechaNacimientoDePersona1A);
+        //otra forma
+        persona1.setFechaNacimiento(LocalDate.of(2000,5,15));
         //
         DocumentoIdentidad dniPersona1 = new DocumentoIdentidad();
         dniPersona1.setNumero("70245555");
         dniPersona1.setTipo("DNI");
         //
 
-        persona1.setFechaNacimiento(fechaNacimientoDePersona1);
+
         persona1.setDocumentoIdentidad(dniPersona1);
 
 
