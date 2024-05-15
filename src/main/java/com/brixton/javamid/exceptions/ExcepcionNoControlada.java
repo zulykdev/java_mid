fa034ad163d417a8a6e62071a799bafbe64d276c
Hans Villaizan <zolymo@gmail.com>
2024-05-14 23:02:22 -0500

    usando try catch en excepciones controladas y no controladas

--- src/main/java/com/brixton/javamid/exceptions/ExcepcionNoControlada.java
+++ src/main/java/com/brixton/javamid/exceptions/ExcepcionNoControlada.java
@@ -27,17 +27,37 @@ public class ExcepcionNoControlada {
             continuar = sc.next();
         }
 
-        int capacity = 6;
-        String [] valores = new String[capacity]; // [0][1][2][3][4][5]
         //input solicitando que caja quiere mostrar...
         // se debe limitar a que la caja que se quiera mostrar no sea mayor al numero total de cajas (6).
-        System.out.println("INGRESE VALOR DE CAJA a mostrar");
-        int inputCaja = sc.nextInt();
-        if (inputCaja <= capacity) {
-            System.out.println("valor de la caja 29:" + valores[inputCaja]);
-        } else {
-            System.out.println("NO ES UN VALOR VALIDO");
+//        int capacity = 6;
+//        String [] valores = new String[capacity]; // [0][1][2][3][4][5]
+//        System.out.println("INGRESE VALOR DE CAJA a mostrar");
+//        int inputCaja = sc.nextInt();
+//        if (inputCaja <= capacity) {
+//            System.out.println("valor de la caja 29:" + valores[inputCaja]);
+//        } else {
+//            System.out.println("NO ES UN VALOR VALIDO");
+//        }
+
+
+        int capacity2 = 6;
+        String [] valores2 = new String[capacity2]; // [0][1][2][3][4][5]
+        //input solicitando que caja quiere mostrar...
+        // se debe limitar a que la caja que se quiera mostrar no sea mayor al numero total de cajas (6).
+        try {
+            System.out.println("INGRESE VALOR DE CAJA a mostrar");
+            int inputCaja2 = sc.nextInt();
+            System.out.println("valor de la caja 29:" + valores2[inputCaja2]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("INGRESAS UN VALOR FUERA DEL RANGO");
+        } catch (Exception e) {
+            System.out.println("SE PRODUJO UN ERROR INESPERADO");
         }
+
+
+
+
+
 
 
     }
