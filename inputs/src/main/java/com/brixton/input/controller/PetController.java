5b5074a39413bf0f36594180d51aadae06f9b5be
Hans Villaizan <zolymo@gmail.com>
2024-05-22 22:55:26 -0500

    added controller and server port

--- inputs/src/main/java/com/brixton/input/controller/PetController.java
+++ inputs/src/main/java/com/brixton/input/controller/PetController.java
@@ -1,8 +1,31 @@
 package com.brixton.input.controller;
 
+import com.brixton.input.dto.request.PetGenericRequestDTO;
+import com.brixton.input.dto.response.PetResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PetController {
+
+    /**
+     * Devuelve le objeto Pet creado.
+     * @return
+     */
+    @GetMapping("/pet")
+    public PetResponseDTO getPets(){
+        log.info("se llamó a getPets ");
+        return new PetResponseDTO();
+    }
+
+
+    @PostMapping("/pet")
+    public void createPet(PetGenericRequestDTO petForCreate){
+        log.info("Valores de Pet recibidos: {}", petForCreate);
+        log.info("fin de mis operaciones");
+    }
 
 }
--- inputs/src/main/java/com/brixton/input/dto/request/PetGenericRequestDTO.java
+++ inputs/src/main/java/com/brixton/input/dto/request/PetGenericRequestDTO.java
@@ -1,8 +1,10 @@
 package com.brixton.input.dto.request;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class PetGenericRequestDTO {
 
     int id;
