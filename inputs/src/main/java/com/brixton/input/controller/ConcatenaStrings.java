aa91d9e7b4e737b968bf7644044597964ff25677
Hans Villaizan <zolymo@gmail.com>
2024-06-02 16:23:59 -0500

    added last changes

---
+++ inputs/src/main/java/com/brixton/input/controller/ConcatenaStrings.java
@@ -0,0 +1,14 @@
+package com.brixton.input.controller;
+
+public class ConcatenaStrings {
+
+    public static void main(String[] args) {
+
+        Integer id = 4;
+        String username = "theUser";
+
+        String concatenado = String.valueOf(id)+"-" + username; //key
+        System.out.println(concatenado);
+
+    }
+}
